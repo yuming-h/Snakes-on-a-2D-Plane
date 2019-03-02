ab2f218aba587c194c62fc96416f32ef372241a2
Annie Wang <annie.wang@hootsuite.com>
2019-03-02 12:29:43 -0800

    modify getpath

--- getPath.js
+++ getPath.js
@@ -1,3 +1,5 @@
+var PF = require("pathfinding");
+
 /**
  * Returns an array of coordinates [{x:n, y:m},...] representing a path to destination.
  * @param {*} state Unaltered game state
@@ -5,22 +7,29 @@
  * @param {x:n, y:m} destination The desired destination
  */
 const getPath = (state, snake, destination) => {
-    const matrix = gernerateMatrix(state.body.);
-}
-
-module.exports = {
-    getPath
-}
+  var matrix = [
+    [0, 0, 0, 1, 0],
+    [1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0]
+  ];
+  var grid = new PF.Grid(matrix);
+  var finder = new PF.AStarFinder();
+  var path = finder.findPath(1, 2, 4, 2, grid);
+  return path;
+  //const matrix = gernerateMatrix(state.body.);
+};
 
 // generate a matrix with
 function gernerateMatrix(board) {
-
-    return matrix;
+  return matrix;
 }
-
 
 // returns a matrix with other snakes as walls
 function addblocks(otherSnake) {
-
-    return matrix;
+  return matrix;
 }
+
+module.exports = {
+  getPath
+};
