8ac2f3969dee45b590d091a18724d6abcd6222af
Annie Wang <annie.wang@hootsuite.com>
2019-03-02 12:51:09 -0800

    modify other function in getpath

--- getPath.js
+++ getPath.js
@@ -6,19 +6,7 @@ var PF = require("pathfinding");
  * @param {*} snake The snake object to find the path for
  * @param {x:n, y:m} destination The desired destination
  */
-const getPath = (state, snake, destination) => {
-  var matrix = [
-    [0, 0, 0, 1, 0],
-    [1, 0, 0, 0, 1],
-    [0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0]
-  ];
-  var grid = new PF.Grid(matrix);
-  var finder = new PF.AStarFinder();
-  var path = finder.findPath(1, 2, 4, 2, grid);
-  return path;
-  //const matrix = gernerateMatrix(state.body.);
-};
+const getPath = (state, snake, destination) => {};
 
 // generate a matrix with
 function gernerateMatrix(board) {
@@ -26,9 +14,20 @@ function gernerateMatrix(board) {
 }
 
 // returns a matrix with other snakes as walls
-function addblocks(otherSnake) {
+function addblocks(matrix, state) {
+  addItself(matrix, state);
+  addOtherSnake(matrix, state);
   return matrix;
 }
+
+//add the snake itself other than the head
+function addItself(matrix, state) {
+  var bodySnake = state.you.body;
+  var removeHead = bodySnake.splice(0, 1);
+  removeHead.forEach(turnZeroToOne(matrix, state));
+}
+
+function turnZeroToOne(matrix, state) {}
 
 module.exports = {
   getPath
