b92c8d2e91fa30b586e824371fd9b5b80e7f12d0
MuchToKnow <justindc99edu@gmail.com>
2019-03-01 23:40:32 -0800

    Possible starter functions

--- index.js
+++ index.js
@@ -28,14 +28,16 @@ app.post('/start', (request, response) => {
   // Response data
   const data = {
     color: '#DFFF00',
+    headType: "smile",
+    tailType: "bolt"
   }
-
   return response.json(data)
 })
 
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
+  const state = request.body
 
   // Response data
   const data = {
@@ -44,6 +46,36 @@ app.post('/move', (request, response) => {
 
   return response.json(data)
 })
+
+/**
+ * Returns an array of coordinates [{x:n, y:m},...] representing a path to destination.
+ * @param {*} state Unaltered game state
+ * @param {*} snake The snake object to find the path for
+ * @param {x:n, y:m} destination The desired destination
+ */
+const getPath = (state, snake, destination) => {
+
+}
+
+/**
+ * Returns if a path is traversable without dying to a snake currently in the way.
+ * This is useful if we are finding paths without regard for other snakes.
+ * We don't need this if we pathfind with something like a*.
+ * @param {*} state
+ * @param {*} path The desired path
+ */
+const isValidPath = (state, path) => {
+
+}
+
+/**
+ * Returns a 2d array of how many moves a particular coordinate will be occupied for.
+ * Used for isValidPath.
+ * @param {*} state
+ */
+const occupiedSquares = (state) => {
+
+}
 
 app.post('/end', (request, response) => {
   // NOTE: Any cleanup when a game is complete.
