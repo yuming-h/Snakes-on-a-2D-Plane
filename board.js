c69a19dbf09c77e6d2088e8c77ac6d5c38250a69
yuming <yumingh7@gmail.com>
2019-03-02 13:45:32 -0800

    board fixes

--- board.js
+++ board.js
@@ -5,24 +5,26 @@ var board
 
 const updateFoodPoints = (food) => {
     for (let foodPoint of food) {
-        let {x} = foodPoint
-        let {y} = foodPoint
+        let x = foodPoint.x
+        let y = foodPoint.y
         board[y][x] = FOOD_MARKER
     }
 }
 const initializeSnakes = (snakes) => {
     for (let snake of snakes) {
-        let {x} = snake
-        let {y} = snake
+        let body = snake.body[0]
+        let x = body.x
+        let y = body.y
         board[y][x] = SNAKE_STARTER_SIZE
     }
 }
 const initializeBoard = (boardData) => {
-    let {width} = boardData
-    let {height} = boardData
+    let width = boardData.board.width
+    let height = boardData.board.height
     board = new Array(height).fill(0).map(() => new Array(width).fill(0))
-    updateFoodPoints(boardData.food)
-    initializeSnakes(boardData.snakes)
+    updateFoodPoints(boardData.board.food)
+    initializeSnakes(boardData.board.snakes)
+    return board
 }
 
 module.exports = {
